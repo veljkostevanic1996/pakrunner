984b241fc0bb200ab5b0c3138f403a0c56f931ef
milos.ivanovic <imilos@gmail.com>
2018-03-09 12:36:24 +0100

    Sredjena metoda getresults

---
+++ d7f88f3e-3754-4132-8c7a-1b41722afd40/kill_processes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 
+# Uzima GUID kao argument i ubija sve procese 
+# koji su otvorili fajl u cijem radnom direktorijumu postoji string GUID
+#
+GUID=$1
+
+PIDS=`lsof -u $USER | grep $GUID | grep cwd | awk '{print $2}'`
+
+kill -6 $PIDS
+echo "Killed processes with PIDs: $PIDS"
+
---
+++ d7f88f3e-3754-4132-8c7a-1b41722afd40/proba.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in `seq 1 100`;
+do
+    echo "Radi $i..."
+    sleep 3
+done
+echo "Zavrseno."
+
