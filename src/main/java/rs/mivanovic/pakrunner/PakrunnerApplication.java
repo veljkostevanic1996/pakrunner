ebc2c77830ae05313e76c2794271f364bfa4e36e
Milos Ivanovic <mivanovic@kg.ac.rs>
2021-06-08 11:02:27 +0200

    Preradjeno pomocu Spring i Swagger.io.

---
+++ src/main/java/rs/mivanovic/pakrunner/PakrunnerApplication.java
@@ -0,0 +1,27 @@
+package rs.mivanovic.pakrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@SpringBootApplication
+public class PakrunnerApplication {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PakrunnerApplication.class, args);
+    }
+
+}
